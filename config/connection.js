f21014614b31dccf9aa748ddae4e2e6e5df1ef8e
Jacob Walton <JacobWalton@Jacobs-MacBook-Pro.local>
2018-07-14 03:01:42 -0500

    Updating Files

---
+++ config/connection.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Swiss Mushroom',FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Baconator',FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Chipotle Ranch',FALSE);
